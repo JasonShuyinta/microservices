67ed61b6ffc440a7115e17dd3af9f934399de5eb
JasonShuyinta <jasonshuyinta@gmail.com>
2023-01-17 14:20:17 +0100

    dockerizing the application

--- cloud-config-server/src/main/java/com/dotjson/microservice/CloudConfigServerApplication.java
+++ cloud-config-server/src/main/java/com/dotjson/microservice/CloudConfigServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableConfigServer
 public class CloudConfigServerApplication {
 
